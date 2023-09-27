58fda0fd5f1e509bfae269df24df5d4e0d95da9a
Reinorr <louis.bouzon@laposte.net>
2023-09-27 10:43:10 +0200

    Exercice 6 : Tableaux
    Each et for each

---
+++ Exercice 6 tableaux/tableaux.js
@@ -0,0 +1,42 @@
+let names = []
+names.push ("Vincent","Paul","Arthur")
+
+names.forEach((element) => {
+    element += "va à la pêche"
+    console.log (element)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//let students = ["Louis","Jean","Gertrude"]
+/* on commence une liste a la place "0", en l'occurence "Jean" est en position 1*/
+//console.log(students[1]) 
+
+//let gameScore =[10,20,30,40,50]
+//gameScore.length
+//for(let i = 0;i <gameScore.length; i++) {
+//    console.log (gameScore[i])
+//}
+//gameScore.push (87)
+//gameScore.splice(1,2)
+//console.log (gameScore)
---
+++ Pierre Feuille Ciseaux/PFC.js
@@ -0,0 +1,1 @@
+let move = ["Pierre","Feuille","Ciseaux"]
