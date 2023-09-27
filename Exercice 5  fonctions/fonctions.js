d2ee36ca1bb8272b0c60ae791f2d2d51882e6fbf
Reinorr <louis.bouzon@laposte.net>
2023-09-27 08:47:43 +0200

    adding exe 5
    Fonctions

---
+++ Exercice 5  fonctions/fonctions.js
@@ -0,0 +1,16 @@
+let a = "Jean"
+let b = "Paul"
+let result = ""
+
+function checkName(a,b) {
+    if (a==b) {
+        result = "identiques"
+    }
+
+    else {
+        result = "pas identiques"
+    }
+console.log (result)
+
+}
+checkName (a,b)
