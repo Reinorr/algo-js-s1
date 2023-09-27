8797967fab3792a49c308b2b4239f1a2289091bd
Reinorr <louis.bouzon@laposte.net>
2023-09-27 11:23:35 +0200

    Exercise 7 objet
    creation of a character sheet for an RPG.

---
+++ Exercices 7 objets/objets.js
@@ -0,0 +1,13 @@
+let jdrCharacter = {
+    firstName : "Reinor ",
+    lastName : "Lawynel ",
+    age : 29,
+    level : 1,
+    class : "Barde",
+    race : "halfling",
+    atk : 15,
+    def : 18,
+    inventory : ["lute","arc court","épée batarde"],
+}
+console.log (jdrCharacter)
+console.log ("Je me nomme "+jdrCharacter.firstName+jdrCharacter.lastName+".Je suis un "+jdrCharacter.race +" "+jdrCharacter.class+" de 29 ans. Je suis niveau "+jdrCharacter.level+" et j'ai "+jdrCharacter.atk+" d'attaque et "+jdrCharacter.def+" de deffense. Voici mon inventaire :"+jdrCharacter.inventory)
